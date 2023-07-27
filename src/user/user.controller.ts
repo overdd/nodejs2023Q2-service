ad58fda1162c7ba44e1b8271a50e6e6a1407fdf7
overdd <lmbib3a4@duck.com>
2023-07-28 00:19:59 +0200

    user controller and service update to work with get and post tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,9 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,27 +19,75 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @HttpCode(HttpStatus.CREATED)
+  create(@Res() response, @Body() createUserDto: CreateUserDto) {
+    try {
+      const user = this.userService.create(createUserDto);
+      return response.status(HttpStatus.CREATED).send(user);
+    } catch (error) {
+      if (error.status === HttpStatus.BAD_REQUEST) {
+        return response.status(HttpStatus.BAD_REQUEST).send();
+      }
+      throw error;
+    }
   }
 
   @Get()
   findAll() {
-    return this.userService.findAll();
+    return [
+      { statusCode: HttpStatus.OK },
+      { users: this.userService.findAll() },
+    ];
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  findOne(@Res() response, @Param('id') id: string) {
+    try {
+      const user = this.userService.findOne(id);
+      return { statusCode: HttpStatus.OK, user };
+    } catch (error) {
+      if (error.status === HttpStatus.NOT_FOUND) {
+        return response.status(HttpStatus.NOT_FOUND).send();
+      }
+      if (error.status === HttpStatus.BAD_REQUEST) {
+        return response.status(HttpStatus.BAD_REQUEST).send();
+      }
+      throw error;
+    }
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    try {
+      const updatedUser = this.userService.update(id, updateUserDto);
+      return { statusCode: HttpStatus.OK, user: updatedUser };
+    } catch (err) {
+      if (err.status === HttpStatus.NOT_FOUND) {
+        return { statusCode: HttpStatus.NOT_FOUND, message: err.message };
+      }
+      if (err.status === HttpStatus.BAD_REQUEST) {
+        return { statusCode: HttpStatus.BAD_REQUEST, message: err.message };
+      }
+      if (err.status === HttpStatus.FORBIDDEN) {
+        return { statusCode: HttpStatus.FORBIDDEN, message: err.message };
+      }
+      throw err;
+    }
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    try {
+      this.userService.remove(id);
+    } catch (err) {
+      if (err.status === HttpStatus.NOT_FOUND) {
+        return { statusCode: HttpStatus.NOT_FOUND, message: err.message };
+      }
+      if (err.status === HttpStatus.BAD_REQUEST) {
+        return { statusCode: HttpStatus.BAD_REQUEST, message: err.message };
+      }
+      throw err;
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from 'src/interfaces/user.interface';
+import { v4 as uuid, validate } from 'uuid';
 
 @Injectable()
 export class UserService {
+  private users: { [id: string]: User } = {};
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const { login, password } = createUserDto;
+
+    if (!login || !password) {
+      throw new BadRequestException('Login and password are required fields');
+    }
+    const creationDate = Date.now();
+    const newUserId = uuid();
+    const newUser: User = {
+      id: newUserId,
+      login,
+      password,
+      version: 1,
+      createdAt: creationDate,
+      updatedAt: creationDate,
+    };
+    this.users[newUserId] = newUser;
+    delete newUser.password;
+    return newUser;
   }
 
   findAll() {
-    return `This action returns all user`;
+    return Object.values(this.users);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    const isUUID = validate(id);
+    if (!isUUID) {
+      throw new BadRequestException('Provided id is not valid');
+    }
+    const user = this.users[id];
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    const user = this.users[id];
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const { oldPassword, newPassword } = updateUserDto;
+
+    if (!oldPassword || !newPassword) {
+      throw new BadRequestException(
+        'Both oldPassword and newPassword are required fields',
+      );
+    }
+
+    if (user.password !== oldPassword) {
+      throw new ForbiddenException('Invalid old password');
+    }
+
+    user.password = newPassword;
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    if (!this.users[id]) {
+      throw new NotFoundException('User not found');
+    }
+    delete this.users[id];
   }
 }
